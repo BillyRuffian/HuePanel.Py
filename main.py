b8da87139967aad33d674f8469cb8f9547575d3a
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2018-01-02 16:51:07 +0000

    working light switches

--- main.py
+++ main.py
@@ -4,50 +4,67 @@ from kivy.properties import ObjectProperty, ListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.togglebutton import ToggleButton
+from kivy.clock import Clock
 
 from hue import Hue
 
 import logging
 
-logger = logging.getLogger( __name__ )
 
-class Lights( StackLayout ):
-    bridge = ObjectProperty( None )
-    group = ObjectProperty( None )
-    
-    def on_group( self, instance, pos ):
-        pass
-
-class GroupPanel( RelativeLayout ):
+class LightSwitch( ToggleButton ):
     pass
 
-class GroupListPanel( BoxLayout ):
-    bridge = ObjectProperty( None )
-    groups = ListProperty([])
-    group = ObjectProperty( None )
-    
-    def on_bridge( self, instance, pos ):
-        groups = self.bridge.groups()
-        for group in list( groups.values() ):
-            self.groups.append( group )
-        self.group = self.groups[0]
-    
-    def on_groups( self, instance, pos ):
-        print( self.groups )
 
-class ControlPanel( Widget ):
-    bridge = ObjectProperty( None )
+class HueControlApp( App ):
     
-    def connect_to_bridge( self ):
+    
+    def build( self ):
+        Clock.schedule_once( self.connect_to_bridge, 3 )
+        
+    
+    def connect_to_bridge( self, dt ):
+        logging.info( 'Connecting to Hue bridge' )
         hue = Hue()
         self.bridge = hue.bridge()
         logging.info( self.bridge )
-
-class HueControlApp( App ):
-    def build( self ):
-        control_panel = ControlPanel()
-        control_panel.connect_to_bridge()
-        return control_panel
+        self.root.ids.screen_manager.current = 'discovering lights'
+        Clock.schedule_once( self.discover_lights, 3 )
+        
+    
+    def discover_lights( self, dt ):
+        self.groups = self.bridge.groups()
+        self.toggles = []
+        for group in self.groups:
+            toggle = LightSwitch( text=self.groups[group]['name'] )
+            toggle.state = 'down' if self.groups[group]['state']['any_on'] == True else 'normal'
+            toggle.bind( on_press=self.toggle_pressed )
+            self.toggles.append( toggle )
+            self.root.ids.lightswitch_layout.add_widget( toggle )
+        self.root.ids.screen_manager.current = 'switches'
+        Clock.schedule_interval( self.refresh_lights, 2.5 )
+        
+    
+    def refresh_lights( self, dt ):
+        # this is super ugly, there must be a better way
+        self.groups = self.bridge.groups()
+        for group in self.groups:
+            for toggle in self.toggles:
+                if self.groups[group]['name'] == toggle.text:
+                    toggle.state = 'down' if self.groups[group]['state']['any_on'] == True else 'normal'
+                    break
+                    
+    
+    def toggle_pressed( self, instance ):
+        # b.groups( 4, 'action', on=True )
+        name = instance.text
+        target_group = None
+        for group in self.groups:
+            if self.groups[group]['name'] == name:
+                self.bridge.groups( group, 'action', on=( instance.state == 'down' ) )
+                break
         
 if __name__ == '__main__':
-    HueControlApp().run()
+    app = HueControlApp()
+    app.run()
